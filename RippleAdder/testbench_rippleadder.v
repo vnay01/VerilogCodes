00b4a028b8f122c06a02c669fd7b07f00ac0a4e5
Vinay Singh <vnay01@gmail.com>
2020-09-03 10:44:47 +0530

    Update testbench_rippleadder.v
    This testbench generates all the possible input combinations (i.e 000 to 111) and corresponding(expected) outputs for a full-adder.

--- RippleAdder/testbench_rippleadder.v
+++ RippleAdder/testbench_rippleadder.v
@@ -4,6 +4,7 @@ module testbenchripple;
 	reg Cin;
 	wire [0:3]S;
 	wire Cout;
+	integer i;
 // instantiate DUT module.
 	ripple_adder RA1(S,Cout,A,B,Cin);
 // Start simulation from here
@@ -11,8 +12,14 @@ module testbenchripple;
 		begin
 			$dumpfile("ripple_adder.vcd");
 			$dumpvars(0,testbenchripple);
+			
+			begin
+			for(i=0;i<8;i++)
+			{A,B,Cin}=i;
+			#5;
 			$monitor($time,"A=%h,B=%h,Cin=%b, S=%h,Cout=%b",A,B,Cin,S,Cout);
-			#5 A=4'b0;B=4'b0;Cin=1'b0;
+			end
+			/*#5 A=4'b0;B=4'b0;Cin=1'b0;
 			#5 A=4'b0;B=4'b0;Cin=1'b1;
 			#5 A=4'b0;B=4'b1;Cin=1'b0;
 			#5 A=4'b0;B=4'b1;Cin=1'b1;
@@ -20,6 +27,7 @@ module testbenchripple;
 			#5 A=4'b1;B=4'b0;Cin=1'b1;
 			#5 A=4'b0;B=4'b1;Cin=1'b0;
 			#5 A=4'b1;B=4'b1;Cin=1'b1;
+			*/
 			#5 $finish;
 		end
 endmodule
