44692240127f8e935db00bcb6d52665b60d5354a
Vinay Singh <vnay01@gmail.com>
2020-12-09 11:28:22 +0530

    Update param_count.v

--- Counters/param_count.v
+++ Counters/param_count.v
@@ -3,7 +3,8 @@
 module counter(input clear, clock,ld,
 			   input [0:N]count_in,
 			   input [0:1]mode,
-			   output reg [0:N]count);
+			   output reg [0:N]count
+			   );
 			   
 	parameter N=7; // 8-bit counter.
 	
@@ -14,5 +15,4 @@ module counter(input clear, clock,ld,
 		else if(mode)		// Mode = 0 - Up count and Mode =1 - Down count
 			 count <=count - 1;
 		else count<= count + 1;
-		
 endmodule
